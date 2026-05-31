d3b973cdd4a7da2a292ca17d1e2afaa6e5a1e96e
Caio-Paccagnella <ra138549@uem.br>
2026-05-31 13:47:27 -0300

    implementa Baralho

---
+++ Baralho.py
@@ -0,0 +1,60 @@
+#Esta biblioteca representa a implementação de um tipo abstrato baralho
+from dataclasses import dataclass
+from enum import Enum
+import random
+
+class Naipe(Enum):
+    OURO = 0
+    ESPADA = 1
+    COPAS = 2
+    PAUS = 3
+
+@dataclass
+class Carta:
+    valor: int
+    naipe: Naipe
+
+class Baralho:
+    '''
+    Representa um baralho de cartas, cujo os valores vão de um à dez e 
+    cada valor possui uma carta de cada Naipe
+    '''
+    cartas: list[Carta]
+
+    def __init__(self):
+        self.cartas = []
+        for i in range(1, 11):
+            self.cartas.append(Carta(i, Naipe.OURO))
+            self.cartas.append(Carta(i, Naipe.COPAS))
+            self.cartas.append(Carta(i, Naipe.ESPADA))
+            self.cartas.append(Carta(i, Naipe.PAUS))
+
+    def embaralha(self) -> None:
+        '''
+        Embaralha as cartas baseada no método Fisher-Yates    
+        '''
+        random.shuffle(self.cartas)
+
+    def retira_uma(self) -> Carta:
+        '''
+        Retira uma carta aleatória do baralho
+
+        OBS: mesmo que idealmente o usuário embaralhe antes, esse
+        método aumenta a aleatoridade
+        '''
+        if len(self.cartas) < 0:
+            return Carta(-1, Naipe.OURO) #Carta inválida para evitar erros
+        index = random.randint(0, len(self.cartas) -1)
+        
+        carta = self.cartas[index]
+        self.cartas[index], self.cartas[-1] = self.cartas[-1], self.cartas[index]
+        self.cartas.pop()
+        return carta
+
+    def reconstroi(self):
+        '''
+        Reconstitui o baralho, retornando as cartas retiradas
+        (Ele volta com a ordem inicial)
+        '''
+    
+        self.__init__()
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Truco-multiplayer
+
 Truco local multiplayer game para a disciplina de Sistemas Operacionais
 
-Alunos:
-Caio César Jordão Paccagnella - RA 138549
-Matheus Henrique Borsato - RA 138246
+| Aluno                         | RA       |
+| ----------------------------- | -------- |
+| Caio César Jordão Paccagnella | RA138549 |
+| Matheus Henrique Borsato      | RA138246 |
